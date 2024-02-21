c6985bd97ca57276da8a384953ba5a3c15df833a
jdmartin <jonsaccount+githubjd@icloud.com>
2024-02-20 21:21:29 -0800

    Path detection tweaks

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,12 +1,25 @@
 #Import Universal Config On Mac
 switch (uname)
-    case Darwin
-        set -gx fish_function_path "/Users/Jon/.config/fish/functions/mac" $fish_function_path
-        source ~/.config/.universal_config
-        ##iTerm integration (Commenting this out seems to have fixed my weird emoji_prompt / backspace issues)
-        source /Users/Jon/.iterm2/.iterm2_shell_integration.fish
-   case Linux
-        set -gx fish_function_path "/Users/Jon/.config/fish/functions/linux" $fish_function_path
+case Darwin
+    set -l mac_functions_path "/Users/Jon/.config/fish/functions/mac"
+    if not contains -- "$mac_functions_path" $fish_function_path
+        set -gx fish_function_path "$mac_functions_path" $fish_function_path
+    end
+    source ~/.config/.universal_config
+    ##iTerm integration (Commenting this out seems to have fixed my weird emoji_prompt / backspace issues)
+    source /Users/Jon/.iterm2/.iterm2_shell_integration.fish
+case Linux
+    set -l mynameis (whoami)
+    switch mynameis
+        case root
+            set -l the_path "/root/"
+        case *
+            set -l the_path "/home/$mynameis/"
+    end
+    set -l linux_functions_path "$the_path/.config/fish/functions/linux"
+    if not contains -- "$linux_functions_path" $fish_function_path
+        set -gx fish_function_path "$linux_functions_path" $fish_function_path
+    end
 end
 
 set -x PROPHOSTNAME (hostname | awk '{print toupper(substr($0,1,1)) substr($0,2)}')
