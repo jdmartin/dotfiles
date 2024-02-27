b1379b75f29632d0438dc46ee0f2b39861e714d9
jdmartin <jonsaccount+githubjd@icloud.com>
2024-02-26 18:12:44 -0800

    Also genericize emoji.fish

--- dot_config/private_fish/conf.d/emoji.fish
+++ dot_config/private_fish/conf.d/emoji.fish
@@ -1,5 +1,5 @@
 #emoji
-set emoji (string split "\n" (cat ~/.config/fish/emoji))
+set emoji (string split "\n" (cat "$HOME"/.config/fish/emoji))
 
 function emoji_badge --on-event fish_prompt
     iterm2_set_user_var badge (random choice $emoji)
