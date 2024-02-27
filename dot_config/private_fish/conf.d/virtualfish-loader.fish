ab7486ac83faf18ff6693a3a1a80586a403dde0b
jdmartin <jonsaccount+githubjd@icloud.com>
2024-02-26 17:52:47 -0800

    Some virtualfish components

---
+++ dot_config/private_fish/conf.d/virtualfish-loader.fish
@@ -0,0 +1,8 @@
+set -g VIRTUALFISH_VERSION 2.5.5
+set -g VIRTUALFISH_PYTHON_EXEC /usr/local/bin/python3
+source /Users/Jon/.local/pipx/venvs/virtualfish/lib/python3.11/site-packages/virtualfish/virtual.fish
+source /Users/Jon/.local/pipx/venvs/virtualfish/lib/python3.11/site-packages/virtualfish/compat_aliases.fish
+source /Users/Jon/.local/pipx/venvs/virtualfish/lib/python3.11/site-packages/virtualfish/projects.fish
+source /Users/Jon/.local/pipx/venvs/virtualfish/lib/python3.11/site-packages/virtualfish/environment.fish
+source /Users/Jon/.local/pipx/venvs/virtualfish/lib/python3.11/site-packages/virtualfish/auto_activation.fish
+emit virtualfish_did_setup_plugins
