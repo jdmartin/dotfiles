fbf901a57cd85f0d649f6c091eb049a0689a155b
jdmartin <jonsaccount+githubjd@icloud.com>
2024-02-27 20:22:07 -0800

    Adding VF was a mistake...

--- dot_config/private_fish/conf.d/virtualfish-loader.fish
+++
@@ -1,8 +0,0 @@
-set -g VIRTUALFISH_VERSION 2.5.5
-set -g VIRTUALFISH_PYTHON_EXEC /usr/local/bin/python3
-source "$HOME"/.local/pipx/venvs/virtualfish/lib/python3.11/site-packages/virtualfish/virtual.fish
-source "$HOME"/.local/pipx/venvs/virtualfish/lib/python3.11/site-packages/virtualfish/compat_aliases.fish
-source "$HOME"/.local/pipx/venvs/virtualfish/lib/python3.11/site-packages/virtualfish/projects.fish
-source "$HOME"/.local/pipx/venvs/virtualfish/lib/python3.11/site-packages/virtualfish/environment.fish
-source "$HOME"/.local/pipx/venvs/virtualfish/lib/python3.11/site-packages/virtualfish/auto_activation.fish
-emit virtualfish_did_setup_plugins
--- dot_config/private_fish/conf.d/virtualfish.fish
+++
@@ -1,5 +0,0 @@
-#virtualfish and venv
-export VIRTUALFISH_HOME="$HOME"/.local/share/virtualenvs
-export VIRTUAL_ENV_DISABLE_PROMPT=false
-export VIRTUALFISH_DEFAULT_PYTHON=/usr/local/bin/python3
-#export VIRTUALFISH_PYTHON_EXEC=/usr/local/bin/python3
