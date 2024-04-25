f6eaee3dbe276a8b9996308d9c54632e7485bce0
jdmartin <jonsaccount+githubjd@icloud.com>
2024-04-25 05:03:26 -0700

    Updated .chezmoiignore

---
+++ dot_config/private_fish/conf.d/fzf.fish
@@ -0,0 +1,26 @@
+# Set up fzf key bindings
+fzf --fish | source
+
+export FZF_CTRL_T_OPTS="
+    --height 60% 
+    --border sharp 
+    --layout reverse 
+    --prompt '∷ ' 
+    --pointer ▶ 
+    --marker ⇒''"
+
+export FZF_CTRL_R_OPTS="
+    --height 60% 
+    --border sharp 
+    --layout reverse 
+    --prompt '∷ ' 
+    --pointer ▶ 
+    --marker ⇒'' 
+    --preview 'echo {1..}'
+    --preview-window up:hidden:wrap 
+    --bind 'ctrl-/:change-preview-window(30%|60%|90%|)'
+    --bind 'ctrl-v:execute(echo {1..} | view - > /dev/tty)' 
+    --bind 'ctrl-t:track+clear-query' 
+    --bind 'ctrl-y:execute-silent(echo -n {1..} | pbcopy)+abort' 
+    --color header:italic 
+    --header 'Press CTRL-Y to copy command into clipboard'"
