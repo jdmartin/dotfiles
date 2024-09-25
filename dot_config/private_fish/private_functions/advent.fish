aac402d74b74fe98f4c69f2eaefbf2899258b598
jdmartin <jonsaccount+githubjd@icloud.com>
2024-09-24 22:10:28 -0700

    Make advent.fish more portable

--- dot_config/private_fish/private_functions/advent.fish
+++ dot_config/private_fish/private_functions/advent.fish
@@ -1,5 +1,6 @@
 function advent --description "Swap to the open-adventure saves dir and run"
-    cd /Users/Jon/Games/!Saves/open-adventure
+    mkdir -p $HOME/Games/!Saves/open-adventure
+    cd $HOME/Games/!Saves/open-adventure
     command /opt/homebrew/bin/advent
     cd -
 end
