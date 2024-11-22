9f7b6087988d776ccf658519481f25a7f0d476f7
jdmartin <jonsaccount+githubjd@icloud.com>
2024-11-21 20:39:07 -0800

    Limit fun emoji to iTerm

--- dot_config/private_fish/conf.d/emoji.fish
+++ dot_config/private_fish/conf.d/emoji.fish
@@ -1,6 +1,8 @@
 #emoji
-set emoji (string split "\n" (cat "$HOME"/.config/fish/emoji))
+if test "$TERM_PROGRAM" = "iTerm.app"
+    set emoji (string split "\n" (cat "$HOME"/.config/fish/emoji))
 
-function emoji_badge --on-event fish_prompt
-    iterm2_set_user_var badge (random choice $emoji)
+    function emoji_badge --on-event fish_prompt
+        iterm2_set_user_var badge (random choice $emoji)
+    end
 end
