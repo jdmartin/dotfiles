958ac8095aa9a0299daa510fb9bd89556c57521c
jdmartin <jonsaccount+githubjd@icloud.com>
2025-05-07 22:19:11 -0700

    Updated advent.fish

--- dot_config/private_fish/private_functions/private_advent.fish
+++ dot_config/private_fish/private_functions/private_advent.fish
@@ -1,6 +1,6 @@
 function advent --description "Swap to the open-adventure saves dir and run"
     mkdir -p $HOME/Games/!Saves/open-adventure
     cd $HOME/Games/!Saves/open-adventure
-    command /opt/homebrew/bin/advent
+    command advent
     cd -
 end
