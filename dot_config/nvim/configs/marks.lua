d157a8029bfe8de983fadecaca6faa8e78939fa7
jdmartin <jonsaccount+githubjd@icloud.com>
2026-02-17 03:55:49 -0800

    Small tweak to marks config

--- dot_config/nvim/configs/marks.lua
+++ dot_config/nvim/configs/marks.lua
@@ -1,6 +1,6 @@
 require'marks'.setup({
   default_mappings = true,
-  builtin_marks = { ".", "<", ">", "^" },
+  builtin_marks = {},
   refresh_interval = 250,
   sign_priority = { lower=10, upper=15, builtin=8, bookmark=20 },
 })
