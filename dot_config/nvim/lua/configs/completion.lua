dec8b60511bf4da814c2a62a8c068bb266ca8002
jdmartin <jonsaccount+githubjd@icloud.com>
2026-05-13 01:36:23 -0700

    Tweaks to menu completion in Neovim

--- dot_config/nvim/lua/configs/completion.lua
+++ dot_config/nvim/lua/configs/completion.lua
@@ -1,5 +1,5 @@
 -- === COMPLETION SETTINGS ===
-vim.opt.completeopt = { "menuone" }
+vim.opt.completeopt = { "menu", "menuone" }
 
 -- Safely require cmp to avoid errors if the plugin isn't installed yet
 local status, cmp = pcall(require, "cmp")
@@ -35,3 +35,22 @@ local cmp_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if cmp_lsp_status then
     _G.lsp_capabilities = cmp_nvim_lsp.default_capabilities()
 end
+
+-- / and ? search completion (buffer words)
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+
+-- : command completion
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    }),
+    matching = { disallow_symbol_nonprefix_matching = false }
+})
